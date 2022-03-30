8918c3390f30e8a9e42d5068e34db9a5c76d882b
Alex <amaduain@gmail.com>
2022-03-30 12:00:37 +0200

    fixing modules

--- code/create_xml.py
+++ code/create_xml.py
@@ -1,6 +1,6 @@
 import logging
 import pytz
-from datetime import datetime
+from datetime import datetime, timedelta
 from influxdb import InfluxDBClient
 
 
@@ -35,9 +35,9 @@ def create_logger(log_level):
 def last_day_of_month(any_day):
     # this will never fail
     # get close to the end of the month for any day, and add 4 days 'over'
-    next_month = any_day.replace(day=28) + datetime.timedelta(days=4)
+    next_month = any_day.replace(day=28) + timedelta(days=4)
     # subtract the number of remaining 'overage' days to get last day of current month, or said programattically said, the previous day of the first of next month
-    return next_month - datetime.timedelta(days=next_month.day)
+    return next_month - timedelta(days=next_month.day)
 
 if __name__ == '__main__':
     logger, log_handler = create_logger(log_level)
